9b80b17d9a7fe496784706fc7becfca0ec46f040
aminwhat <m.aminwhat@gmail.com>
2025-01-12 12:51:32 +0330

    done the first part

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,31 @@
 mod data;
+mod inference;
 mod model;
 mod training;
 
-use crate::model::ModelConfig;
-use burn::backend::Wgpu;
+use crate::{model::ModelConfig, training::TrainingConfig};
+use burn::{
+    backend::{Autodiff, Wgpu},
+    data::dataset::Dataset,
+    optim::AdamConfig,
+};
 
 fn main() {
     type MyBackend = Wgpu<f32, i32>;
+    type MyAutodiffBackend = Autodiff<MyBackend>;
 
-    let device = Default::default();
-    let model = ModelConfig::new(10, 512).init::<MyBackend>(&device);
-
-    println!("{}", model);
+    let device = burn::backend::wgpu::WgpuDevice::default();
+    let artifact_dir = "/tmp/guide";
+    crate::training::train::<MyAutodiffBackend>(
+        artifact_dir,
+        TrainingConfig::new(ModelConfig::new(10, 512), AdamConfig::new()),
+        device.clone(),
+    );
+    crate::inference::infer::<MyBackend>(
+        artifact_dir,
+        device,
+        burn::data::dataset::vision::MnistDataset::test()
+            .get(42)
+            .unwrap(),
+    );
 }
